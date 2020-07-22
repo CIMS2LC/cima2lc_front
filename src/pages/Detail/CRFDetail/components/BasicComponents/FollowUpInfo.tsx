007d356042440a98d98b226d1355626546088f2f
chenkarl <1209031611@qq.com>
2020-07-22 17:13:08 +0800

    1

--- src/pages/Detail/CRFDetail/components/BasicComponents/FollowUpInfo.tsx
+++ src/pages/Detail/CRFDetail/components/BasicComponents/FollowUpInfo.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import EditableTable from '@/pages/BasicComponents/EditableTable';
-import { DatePicker, Select, Upload, Button } from 'antd';
+//import EditableTable from '@/pages/BasicComponents/EditableTable';
+import { Form, Button, Space, DatePicker, Select, Popconfirm } from 'antd';
 import PicturesWall from './PicturesWall';
-import { UploadOutlined } from '@ant-design/icons';
+import {
+  MinusCircleOutlined,
+  PlusOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import moment from 'moment';
 
 const fw_Options = [
   { label: '电话', value: 1 },
@@ -29,69 +34,131 @@ const it_Options = [
   { label: 'PET/CT', value: 5 },
 ];
 
-class FollowUpInfo extends React.Component {
-  constructor(props) {
-    super(props);
+var data = [];
+const onFinish = values => {
+  console.log(values);
+  if (values.FollInfo) {
+    for (let j = 0, len = values.FollInfo.length; j < len; j++) {
+      values.FollInfo[j].date = moment(values.FollInfo[j].date).format(
+        'YYYY-MM-DD',
+      );
+    }
+    console.log(values.FollInfo);
   }
+};
+
+class FollowUpInfo extends React.Component {
   state = {};
+
   render() {
     return (
-      <div>
-        <EditableTable
-          dataColumns={[
-            {
-              title: '序号',
-              dataIndex: 'order',
-              width: '5%',
-            },
-            {
-              title: '随访日期',
-              key: 'followup_time',
-              width: '10%',
-              render: () => <DatePicker />,
-            },
-            {
-              title: '随访方式',
-              key: 'followup_way',
-              width: '10%',
-              render: () => (
-                <Select style={{ width: 120 }} options={fw_Options} />
-              ),
-            },
-            {
-              title: '疗效评估',
-              dataIndex: 'response_evaluation',
-              width: '10%',
-              render: () => (
-                <Select style={{ width: 120 }} options={re_Options} />
-              ),
-            },
-            {
-              title: '生存状态',
-              dataIndex: 'live_state',
-              width: '10%',
-              render: () => (
-                <Select style={{ width: 120 }} options={ls_Options} />
-              ),
-            },
-            {
-              title: '影像类型',
-              dataIndex: 'image_type',
-              width: '10%',
-              render: () => (
-                <Select style={{ width: 120 }} options={it_Options} />
-              ),
-            },
-            {
-              title: '影像',
-              dataIndex: 'image',
-              width: '20%',
-              render: () => <PicturesWall />,
-            },
-          ]}
-          operColumns={[]}
-        />
-      </div>
+      <Form name="form" onFinish={onFinish} autoComplete="off">
+        <Form.List name="FollInfo">
+          {(fields, { add, remove }) => {
+            return (
+              <div>
+                {fields.map(field => (
+                  <Space
+                    key={field.key}
+                    style={{ display: 'flex', marginBottom: 8 }}
+                    align="start"
+                  >
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'key']}
+                      fieldKey={[field.fieldKey, 'key']}
+                      label="序号"
+                    >
+                      {field.key + 1}
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'date']}
+                      fieldKey={[field.fieldKey, 'date']}
+                      rules={[{ required: true, message: '缺少随访日期' }]}
+                      label="随访日期"
+                    >
+                      <DatePicker format="YYYY-MM-DD" />
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'folMet']}
+                      fieldKey={[field.fieldKey, 'folMet']}
+                      rules={[{ required: true, message: '缺少随访方式' }]}
+                      label="随访方式"
+                    >
+                      <Select style={{ width: 120 }} options={fw_Options} />
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'effEva']} // key={field.key}
+                      fieldKey={[field.fieldKey, 'effEva']}
+                      rules={[{ required: true, message: '缺少随访方式' }]}
+                      label="疗效评估"
+                    >
+                      <Select style={{ width: 120 }} options={re_Options} />
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'livSta']}
+                      fieldKey={[field.fieldKey, 'livSta']}
+                      rules={[{ required: true, message: '缺少生存状态' }]}
+                      label="生存状态"
+                    >
+                      <Select style={{ width: 120 }} options={ls_Options} />
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'imaFilType']}
+                      fieldKey={[field.fieldKey, 'imaFilType']}
+                      label="影像类型"
+                    >
+                      <Select style={{ width: 120 }} options={it_Options} />
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'savFilPath']}
+                      fieldKey={[field.fieldKey, 'savFilPath']}
+                      label="影像"
+                    >
+                      <PicturesWall />
+                    </Form.Item>
+
+                    <Popconfirm
+                      title="确认删除?"
+                      onConfirm={() => {
+                        remove(field.name);
+                        data.splice(field.name, 1);
+                      }}
+                    >
+                      <MinusCircleOutlined />
+                    </Popconfirm>
+                  </Space>
+                ))}
+
+                <Form.Item>
+                  <Button
+                    type="dashed"
+                    onClick={() => {
+                      add();
+                      data.push({});
+                    }}
+                    block
+                  >
+                    <PlusOutlined />
+                  </Button>
+                </Form.Item>
+              </div>
+            );
+          }}
+        </Form.List>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            保存
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
